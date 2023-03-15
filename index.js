0f9c141f5552bb45e287e7d2f58397ccfcf7d405
Rubby <rubenslombard@pursuit.org>
2023-03-15 10:04:25 -0400

    lab-creating-updating-dom-element-homework

---
+++ index.js
@@ -0,0 +1,54 @@
+const section = document.querySelector("section");
+section.classList.add("featured");
+
+
+const postClass = document.querySelector(".posts");
+const article = document.createElement("article");
+postClass.append("article");
+
+ console.log(section);
+
+// const selectArticle = document.querySelector(`article`);
+
+// const img = document.createElement(`img`);
+// img.setAttribute(`src`, "./images/paul-gilmore-unsplash.jpg");
+// img.setAttribute(`alt`, "Image of a mountain in front of a lake.");
+// selectArticle.prepend(`img`);
+
+// const heading = document.createElement(`h3`);
+// heading.innerText = `Stop Planning`;
+
+// selectArticle.append(`heading`);
+
+// const paragraph = document.createElement(`p`);
+// paragraph.innerText = `You -- yes you! You're an over-planner, I can tell. It's time to stop
+//     planning so much and instead focusing on relaxing. Taking a break at all
+//     is so stressful these days; why add to the stress by overworking yourself?`;
+
+// selectArticle.append(`paragraph`);
+
+// const aside = document.createElement(`aside`);
+// selectArticle.append(`aside`);
+
+// const selectAside = document.querySelector(`aside`);
+// let paragraph2 = document.createElement(`p`);
+// selectAside.prepend(`paragraph2`);
+
+// const selectParagraph = document.querySelector(`paragraph2`);
+// const span = document.createElement(span);
+// selectParagraph.prepend(span);
+// span.innerText = `4 Minutes`;
+
+// const selectSpan = document.querySelector(`span`);
+// const strong = document.createElement(`strong`);
+// strong.innerText = `Read Time:`;
+// selectSpan.prepend(`strong`);
+
+// const link = document.createElement(`a`);
+// link.setAttribute(`href`, "#");
+// link.innerText = `Read more...`
+// selectParagraph.append(`link`)
+
+
+
+
