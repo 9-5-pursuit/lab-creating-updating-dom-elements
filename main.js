97c105025212410bf84b0fe80f625e632a2153e6
charellpeterkin <119958341+charellpeterkin@users.noreply.github.com>
2023-03-15 10:33:43 -0400

    second attempt

--- main.js
+++ main.js
@@ -6,27 +6,27 @@ let section = document.querySelector("section.posts");
 section.append(article)
 console.log(section)
 
-let image = document.createElement("img");
-image.setAttribute("src", "./images/paul-gilmore-unsplash.jpg");
-image.setAttribute("alt", "Image of a mountain in front of a lake.");
-article.prepend(image)
+let img = document.createElement("img");
+img.setAttribute("src", "./images/paul-gilmore-unsplash.jpg");
+img.setAttribute("alt", "Image of a mountain in front of a lake.");
+article.prepend(img)
 
-let header = document.createElement("h3");
-header.textContent = "Stop Planning"
-article.append(header)
-
-let paragraph = document.createElement("p");
-header.after(paragraph)
-paragraph.textContent = "You -- yes you! You're an over-planner, I can tell. It's time to stop planning so much and instead focusing on relaxing. Taking a break at all is so stressful these days; why add to the stress by overworking yourself?"
-
-let aside = document.createElement("aside");
-paragraph.after(aside)
+let h3 = document.createElement("h3");
+h3.textContent = "Stop Planning"
+article.append(h3)
 
 let p = document.createElement("p");
-aside.prepend(p);
+p.textContent = "You -- yes you! You're an over-planner, I can tell. It's time to stop planning so much and instead focusing on relaxing. Taking a break at all is so stressful these days; why add to the stress by overworking yourself?"
+article.append(p)
+
+let aside = document.createElement("aside");
+article.append(aside)
+
+let p1 = document.createElement("p");
+aside.prepend(p1);
 
 let span = document.createElement("span");
-p.prepend(span);
+p1.prepend(span);
 span.textContent = "<strong>Read Time:<strong>"
 span.textContent += "4 Minutes"
 
@@ -35,4 +35,6 @@ a.textContent = "Read More"
 a.setAttribute("href", "#")
 span.append(a)
 
-let first = document.querySelector("section.post article")
+let first = document.querySelector("section.posts").append(article)
+let second = document.querySelector("section.posts article:nth-child(2)")
+document.querySelector("section.posts").prepend(second)
