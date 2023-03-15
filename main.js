ab5217da9ed94eb7f4d4dc489419f8743f44fd7e
Blui322 <luistejada@pursuit.com>
2023-03-15 09:54:41 -0400

    almost

---
+++ main.js
@@ -0,0 +1,41 @@
+const section = document.querySelector("section");
+section.setAttribute("class", "featured");
+
+
+const article = document.createElement("article");
+const art1 = document.querySelector("section.posts");
+art1.append(article);
+
+const img = document.createElement("img");
+article.prepend(img);
+img.setAttribute("src", "./images/paul-gilmore-unsplash.jpg");
+img.setAttribute("alt", "Image of a mountain in front of a lake.");
+
+const h3 = document.createElement("h3");
+article.append(h3);
+h3.textContent = "Stop Planning";
+
+const pi = document.createElement("p");
+article.append(pi);
+pi.textContent = "You -- yes you!";
+
+const aside = document.createElement("aside");
+article.append(aside);
+
+const pe = document.createElement("p");
+aside.append(pe)
+ 
+const span = document.createElement("span");
+pe.prepend(span);
+span.textContent = "4 Minutes";
+
+const strong = document.createElement("strong");
+span.prepend(strong);
+strong.textContent = "Read Time ";
+ 
+const a = document.createElement("a");
+pe.append(a);
+a.textContent = "Read more";
+a.setAttribute("href", "#");
+
+
