e36dffde3124ecec8c42323637741231956f6746
Rubby <rubenslombard@pursuit.org>
2023-04-06 22:44:13 -0400

    lab-creating-updating-dom-element-1- complete

--- index.js
+++ index.js
@@ -1,54 +1,36 @@
+
 const section = document.querySelector("section");
 section.classList.add("featured");
 
+ const article = document.querySelector("article");
 
-const postClass = document.querySelector(".posts");
-const article = document.createElement("article");
-postClass.append("article");
+ const img = document.createElement("img");
+    img.setAttribute("src", "./images/paul-gilmore-unsplash.jpg");
+    img.setAttribute("alt", "Image of a mountain in front of a lake.");
+    article.prepend(img);
 
- console.log(section);
+ const h3 = document.createElement("h3");
+    h3.innerText = "Stop Planning";
 
-// const selectArticle = document.querySelector(`article`);
+     article.append(h3);
 
-// const img = document.createElement(`img`);
-// img.setAttribute(`src`, "./images/paul-gilmore-unsplash.jpg");
-// img.setAttribute(`alt`, "Image of a mountain in front of a lake.");
-// selectArticle.prepend(`img`);
+ const p = document.createElement("p");
+ p.innerText = `You -- yes you! You're an over-planner, I can tell. It's time to stop
+    planning so much and instead focusing on relaxing. Taking a break at all
+    is so stressful these days; why add to the stress by overworking yourself?`;
 
-// const heading = document.createElement(`h3`);
-// heading.innerText = `Stop Planning`;
+ article.append(p);
 
-// selectArticle.append(`heading`);
+const aside = document.createElement("aside")
+aside.innerHTML = `<p>
+      <span><strong>Read Time:</strong> 4 Minutes</span
+      ><a href="#">Read more...</a>
+    </p>`;
+article.append(aside)
+document.querySelector("section.posts").append(article);
 
-// const paragraph = document.createElement(`p`);
-// paragraph.innerText = `You -- yes you! You're an over-planner, I can tell. It's time to stop
-//     planning so much and instead focusing on relaxing. Taking a break at all
-//     is so stressful these days; why add to the stress by overworking yourself?`;
-
-// selectArticle.append(`paragraph`);
-
-// const aside = document.createElement(`aside`);
-// selectArticle.append(`aside`);
-
-// const selectAside = document.querySelector(`aside`);
-// let paragraph2 = document.createElement(`p`);
-// selectAside.prepend(`paragraph2`);
-
-// const selectParagraph = document.querySelector(`paragraph2`);
-// const span = document.createElement(span);
-// selectParagraph.prepend(span);
-// span.innerText = `4 Minutes`;
-
-// const selectSpan = document.querySelector(`span`);
-// const strong = document.createElement(`strong`);
-// strong.innerText = `Read Time:`;
-// selectSpan.prepend(`strong`);
-
-// const link = document.createElement(`a`);
-// link.setAttribute(`href`, "#");
-// link.innerText = `Read more...`
-// selectParagraph.append(`link`)
-
-
+const secondPost = document.querySelector("section.posts article:nth-child(2)")
+    
+document.querySelector(`section.posts`).prepend(secondPost);
 
 
